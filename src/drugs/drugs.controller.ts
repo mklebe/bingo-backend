7828fbec693cf994c3c608e567b4d1b2e6d96d7a
Matthias Klebe <matthias.klebe@gmail.com>
2021-08-01 08:21:34 +0200

    Adds endpoints for song categories

--- src/drugs/drugs.controller.ts
+++ src/drugs/drugs.controller.ts
@@ -2,7 +2,21 @@ import { HttpService } from '@nestjs/axios';
 import { Controller, Get, Param } from '@nestjs/common';
 import { SearchService } from 'src/search/search.service';
 
-const { JSDOM } = require('jsdom') 
+function getSongListUrl(start: string, end: string) {
+  return `https://playlist.funtip.de/playList.do?action=searching&remote=1&version=2&from=${start}&to=${end}&jsonp_callback=jQuery224044240703639644585_1627199132642&_=1627199132643`
+}
+
+const { JSDOM } = require('jsdom')
+const categoryUrl: Record<string, string> = {
+  'Top100Family': getSongListUrl('27-06-2021_09-00', '27-06-2021_19-00'),
+  'Top100Numbers': getSongListUrl('04-07-2021_09-00', '04-07-2021_19-00'),
+  'Top100Animals': getSongListUrl('11-07-2021_09-00', '11-07-2021_19-00'),
+  'Top100Drugs': getSongListUrl('18-07-2021_09-00', '18-07-2021_19-00'),
+  'Top100Eighties': getSongListUrl('25-07-2021_09-00', '25-07-2021_19-00'),
+  'Top100Mobility': getSongListUrl('01-08-2021_09-00', '01-08-2021_19-00'),
+  'Top100Instrumentals': getSongListUrl('08-08-2021_09-00', '08-08-2021_19-00'),
+  'Test': getSongListUrl('01-08-2021_06-00', '01-08-2021_08-00')
+}
 
 @Controller('songlist')
 export class DrugsController {
@@ -15,18 +29,50 @@ export class DrugsController {
   async searchSong(@Param() params) {
     return this.searchService.searchSong(params.category, params.artist, params.song).then((result) => {
       if(result?.body?.hits?.hits) {
-        return result.body.hits.hits.map((item) => item._source )
+        return result.body.hits.hits.map((item) => item )
       } else {
         return {}
       }
     })
   }
 
+  @Get(':category')
+  async getCategoryByName(@Param() params) {
+    return new Promise((resolve, reject) => {
+      const catUrl: string = categoryUrl[params.category] || categoryUrl['Top100Eighties'] 
+      
+      this.httpService
+        .get(catUrl, {
+          responseType: 'arraybuffer'
+        })
+        .subscribe(( response ) => {
+          let listScript: string = response.data.toString('latin1')
+          console.log(listScript)
+          const top100Table = new RegExp('<table(.|\n)*?<\/table>')
+          let currentPosition = 100;
+  
+          const { document } = new JSDOM( listScript.match(top100Table)[0] ).window;
+          const top100List = [... document.querySelectorAll('tr td:nth-child(2)')]
+          let result = top100List.map((tableRow) => {
+            const songRow = tableRow.textContent.split('\\n                ')[2].replace('        ', '')
+            return {
+              placement: currentPosition--,
+              artist: songRow.split(' — ')[0],
+              song: songRow.split(' — ')[1],
+            }
+          })
+          resolve(result);
+        })
+    });
+  }
+
   @Get()
   async getAll() {
     return new Promise((resolve, reject) => {
       this.httpService
-        .get('https://playlist.funtip.de/playList.do?action=searching&remote=1&version=2&from=25-07-2021_09-00&to=25-07-2021_19-00&jsonp_callback=jQuery224044240703639644585_1627199132642&_=1627199132643')
+        .get(categoryUrl['Test'], {
+          responseType: 'arraybuffer'
+        })
         .subscribe(( response ) => {
           let listScript: string = response.data
           const top100Table = new RegExp('<table(.|\n)*?<\/table>')
